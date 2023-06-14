f5d09fcb12abddae94d3bdea63986a2f49b468d6
operat04 <operat04@naver.com>
2023-06-14 15:16:43 +0900

    Signin 페이지 폼 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Signin from "./pages/Signin";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Signin></Signin>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Signin.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  input {
+    margin: 10px 0;
+    width: 250px;
+  }
+  div {
+    color: gray;
+    font-size: 15px;
+  }
+  button {
+    margin: 10px 0 5px 0;
+  }
+`;
+
+const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    console.log(email);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    console.log(password);
+  };
+
+  return (
+    <InputContainer>
+      <h1>Signin Page</h1>
+      <input
+        data-testid="email-input"
+        placeholder="이메일을 입력해주세요"
+        onChange={handleEmail}
+      ></input>
+      {email.includes("@") ? (
+        <div></div>
+      ) : (
+        <div>이메일 형식에 맞게 입력해주세요.</div>
+      )}
+      <input
+        data-testid="password-input"
+        placeholder="비밀번호를 입력해주세요"
+        onChange={handlePassword}
+        type="password"
+      ></input>
+      {password.length < 8 ? (
+        <div>비밀 번호를 8자리 이상 입력해주세요</div>
+      ) : (
+        <div></div>
+      )}
+      <button
+        data-testid="signin-button"
+        disabled={password.length < 8 || !email.includes("@")}
+      >
+        로그인
+      </button>
+      <button>회원가입</button>
+    </InputContainer>
+  );
+};
+
+export default Signin;
