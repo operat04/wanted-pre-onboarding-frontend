31c1697944afef3e197f21220fa75b5e321405eb
operat04 <operat04@naver.com>
2023-06-14 15:33:26 +0900

    로그인,회원가입 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Signin from "./pages/SigninPage";
+import Signup from "./pages/SignupPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
