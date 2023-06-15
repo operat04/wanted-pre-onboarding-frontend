d752e395a9c59413e9fc3728f6f30d57796a5fcc
operat04 <operat04@naver.com>
2023-06-15 17:37:34 +0900

    초기 화면 로그인 화면으로 지정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Signin from "./pages/SigninPage";
 import Signup from "./pages/SignupPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import TodosPage from "./pages/TodosPage";
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signin"></Navigate>}></Route>
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/todo" element={<TodosPage />} />
